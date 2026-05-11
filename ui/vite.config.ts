76941ab371ce8182a24ef6f032af316842f32916
mrkadirov2005 <muzaffar571181@gmail.com>
2026-05-12 02:08:56 +0500

    done

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     port: 5173, // frontend dev server
     proxy: {
       '/api': {
-        target: 'http://localhost:4000', // local backend
+        target: 'https://temurbek.m-kadirov.uz', // backend
         changeOrigin: true,
       },
     },
